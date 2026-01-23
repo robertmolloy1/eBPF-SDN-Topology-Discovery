4222836e3b49b1727a4888f27ccd1749fb031e3b
Robert Molloy <robertmolloy100@gmail.com>
2026-01-23 22:05:32 +0000

    Finish controller - Add link remover loop - pre-generate LLDP packets - minor fix3es and cleanup

--- controller/topology_discovery.py
+++ controller/topology_discovery.py
@@ -17,22 +17,46 @@ class MainCLI(cmd.Cmd):
         self.application = application
 
     def do_discover_topology(self, line):
-        Thread(target=self.application.lldp_generator).start()
+        if self.application.discovery_thread and self.application.discovery_thread.is_alive():
+            print("Discovery process already running")
+            return
+
+        lldp_thread = Thread(target=self.application.lldp_generator)
+        self.application.discovery_thread = lldp_thread
+        self.application.threads.append(lldp_thread)
+
+        link_remover_thread = Thread(target=self.application.link_remover)
+        self.application.link_remover_thread = link_remover_thread
+        self.application.threads.append(link_remover_thread)
+
+        lldp_thread.start()
+        link_remover_thread.start()
+        print("Discovery process started")
+
 
     def do_print_topology(self,line):
-        for src_sw in self.application.links:
-            for src_port, (dst_sw,dst_port) in self.application.links[src_sw].items():
+        # print(self.application.links)
+        for src_sw in self.application.connections:
+            for src_port, (dst_sw,dst_port,_time) in self.application.links[src_sw].items():
                 print(f'Link: Switch {src_sw}, port {src_port} ---> switch {dst_sw}, port {dst_port}')
 
         for host,(switch,connected_port) in self.application.hosts.items():
             print(f'Host {host.hex(":")} connected to switch {switch}, port {connected_port}')
 
 
-class LearningSwitchApplication(eBPFCoreApplication):
+class TopologyDiscoveryApplication(eBPFCoreApplication):
 
-    switch_ports = {}
-    links = {}
-    hosts = {}
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,**kwargs)
+        self.active = True
+
+        self.switch_ports = {}
+        self.links = {}
+        self.hosts = {}
+
+        self.threads = []
+        self.discovery_thread = None
+        self.link_remover_thread = None
 
     def run(self):
         Thread(target=reactor.run, kwargs={'installSignalHandlers': 0}).start()
@@ -42,13 +66,31 @@ class LearningSwitchApplication(eBPFCoreApplication):
         except KeyboardInterrupt:
             print("\nGot keyboard interrupt. Exiting...")
         finally:
+            self.active = False
             reactor.callFromThread(reactor.stop)
+            for t in self.threads:
+                t.join(timeout=5)
 
     @set_event_handler(Header.HELLO)
     def hello(self, connection, pkt):
-        self.switch_ports[connection.dpid] = pkt.port_count
+        self.switch_ports[connection.dpid] = {}
         self.links[connection.dpid] = {}
 
+        # Generate LLDP packet for each port in advance 
+        LLDP_DST = "01:80:c2:00:00:0e"
+        for i in range(pkt.port_count):
+            pkt = (
+                Ether(dst=LLDP_DST, src=LLDP_DST, type=0x88cc)
+                /    LLDPDUChassisID(subtype=7, id=struct.pack(">Q",connection.dpid))
+                /    LLDPDUPortID(subtype=7, id=str(i))
+                /    LLDPDUTimeToLive(ttl=120)
+                /    LLDPDUEndOfLLDPDU()
+            )
+            pkt_bytes = b"\x00" * 14 + bytes(pkt) # Pad beginning of packet as BPFabric expects 14 bytes of metadata 
+
+            self.switch_ports[connection.dpid][i] = pkt_bytes
+
+        # Install eBPF functions
         with open('../topology_discovery/Topology_Discovery_v1.o', 'rb') as f:
             print("Installing the topology discovery eBPF ELF")
             connection.send(FunctionAddRequest(name="topologydiscovery", index=0, elf=f.read()))
@@ -123,27 +165,32 @@ class LearningSwitchApplication(eBPFCoreApplication):
         for host in hosts_to_delete:
             del self.hosts[host]
         
-        self.links.setdefault(src_dpid, {})[src_port] = (dst_dpid,dst_port)
+        self.links.setdefault(src_dpid, {})[src_port] = (dst_dpid,dst_port,time.time())
 
     def lldp_generator(self):
-        while True:
-            for switch, ports in self.switch_ports.items():
-                for i in range(ports):
-                    LLDP_DST = "01:80:c2:00:00:0e"
-                    pkt = (
-                        Ether(dst=LLDP_DST, src=LLDP_DST, type=0x88cc)
-                        /    LLDPDUChassisID(subtype=7, id=struct.pack(">Q",switch))
-                        /    LLDPDUPortID(subtype=7, id=str(i))
-                        /    LLDPDUTimeToLive(ttl=120)
-                        /    LLDPDUEndOfLLDPDU()
-                    )
-                    pkt_bytes = b"\x00" * 14 + bytes(pkt)
-                    eth_dst, eth_src, eth_type = struct.unpack_from('>6s6sH', pkt_bytes, struct.calcsize('I10x'))
+        while self.active:
+            for switch in self.connections:
+                for port, lldp_packet in self.switch_ports[switch].items():
+                    self.connections[switch].send(PacketOut(data=lldp_packet, out_port=port))
 
-                    self.connections[switch].send(PacketOut(data=pkt_bytes, out_port=i))
+            time.sleep(5)
+
+    def link_remover(self):
+        while self.active:
+            time_now = time.time()
+            links_to_delete = []
+            for switch in self.connections:
+                for port, value in self.links[switch].items():
+                    if time_now - value[2] >10:
+                        links_to_delete.append((switch,port))
+
+            for link in links_to_delete:
+                del self.links[link[0]][link[1]]
 
             time.sleep(5)
 
 
+
+
 if __name__ == '__main__':
-    LearningSwitchApplication().run()
+    TopologyDiscoveryApplication().run()
